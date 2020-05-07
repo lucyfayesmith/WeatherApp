616c22f1092d74784faf3ade5025e43d584cfa14
Eleni Ioakeim <eioake01@cs.ucy.ac.cy>
2020-05-07 17:02:17 +0200

    Create navigation menu and add new location functionality

---
+++ app/src/main/java/com/example/weatherapp/NavigationMainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.weatherapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.db.entity.Location
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_navigation_main.*
+
+
+class NavigationMainActivity : AppCompatActivity() {
+
+    private lateinit var locationViewModel: LocationViewModel
+    private val newLocationActivityRequestCode = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navView = findViewById<View>(R.id.navigation_view) as NavigationView
+
+        createMenu(navView)
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.groupId) {
+                R.id.misc -> {
+                    if (it.title == "Add Location") {
+                        val intent = Intent(this@NavigationMainActivity, NewLocationActivity::class.java)
+                        startActivityForResult(intent, newLocationActivityRequestCode)
+                    }
+                }
+                R.id.locations -> {
+                    Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
+                }
+            }
+            drawer.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    //Helps with handling any action implemented in the toolbar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    fun createMenu(navView: NavigationView) {
+        val menu = navView.menu
+        menu.clear()
+        navView.invalidate()
+
+        locationViewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+        locationViewModel.allLocations.observe(this, Observer { locations ->
+            locations?.let {
+                var i = 0
+                for (location in it) {
+                        val item: MenuItem = menu.add(R.id.locations, i, 0, it.get(i).location)
+                        item.setIcon(R.mipmap.ic_launcher)
+                        item.isCheckable = true
+                        i++
+
+                }
+            }
+        })
+
+        val item: MenuItem = menu.add(R.id.misc, 0, 1, "Add Location")
+        item.setIcon(R.drawable.ic_add)
+        item.isCheckable = true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newLocationActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewLocationActivity.EXTRA_REPLY)?.let {
+                val location = Location(it)
+                locationViewModel.insert(location)
+            }
+        } else {
+            Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/weatherapp/NewLocationActivity.java
@@ -0,0 +1,39 @@
+package com.example.weatherapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewLocationActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.weatherapp.REPLY";
+
+    private EditText mEditLocationView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_location);
+        mEditLocationView = findViewById(R.id.edit_location);
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(mEditLocationView.getText())) {
+                    setResult(RESULT_CANCELED, replyIntent);
+                } else {
+                    String word = mEditLocationView.getText().toString();
+                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+}
