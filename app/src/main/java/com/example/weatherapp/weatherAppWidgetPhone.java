75f52e8f8e32a2525d69187dca3a4017f6cda8e6
jjsanda <jsanda@students.zcu.cz>
2020-05-16 11:58:34 +0200

    Added counting of updates and show of widget id

--- app/src/main/java/com/example/weatherapp/weatherAppWidgetPhone.java
+++ app/src/main/java/com/example/weatherapp/weatherAppWidgetPhone.java
@@ -1,22 +1,64 @@
 package com.example.weatherapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class weatherAppWidgetPhone extends AppWidgetProvider {
 
+    private static final String mSharedPrefFile =
+            "com.example.android.appwidgetsample";
+    private static final String COUNT_KEY = "count";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        shared instance
+        SharedPreferences prefs = context.getSharedPreferences(
+                mSharedPrefFile, 0);
+//        count update
+        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
+
+//          get date for logic of last update
+        String dateString =
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
+
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_app_widget_phone);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+//        set date to last update (view text)
+        views.setTextViewText(R.id.appwidget_update,
+                context.getResources().getString(
+                        R.string.date_count_format, count, dateString));
+
+        SharedPreferences.Editor prefEditor = prefs.edit();
+        prefEditor.putInt(COUNT_KEY + appWidgetId, count);
+        prefEditor.apply();
+
+        Intent intentUpdate = new Intent(context, weatherAppWidgetPhone.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
